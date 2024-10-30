b6e0e647b683025ba7f1f42693bbf2ac2d743e16
bfixman <122493860+bfixman@users.noreply.github.com>
2024-10-29 19:54:21 -0700

    Add files via upload

---
+++ A3A_Exogenous/Scripts/SigProfilerExtractor_RNA.py
@@ -0,0 +1,8 @@
+from SigProfilerExtractor import sigpro as sig
+def main_function():    
+   # to get input from table format (mutation catalog matrix)
+   path_to_example_table = sig.importdata("matrix")
+   data = "/project/xiaojiac_1206/SigProfilerExtractor/a3a_rna_matrices/transposed_matrix.tsv"
+   sig.sigProfilerExtractor("matrix", "A3A_RNA_output", data, opportunity_genome="GRCh38", minimum_signatures=1, maximum_signatures=3)
+if __name__=="__main__":
+   main_function()
